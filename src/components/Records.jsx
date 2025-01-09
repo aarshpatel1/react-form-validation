e627e27719f7cfa5014262dd52cb4efb814900ad
aarshpatel1 <aarsh18patel@gmail.com>
2025-01-09 15:40:11 +0530

    initial commit

---
+++ src/components/Records.jsx
@@ -0,0 +1,47 @@
+function Records({ usersList }) {
+	return (
+		<>
+			<div className="container">
+				<h2 className="text-center my-5">User Records</h2>
+				<table className="table table-hover my-5">
+					<thead>
+						<tr>
+							<th scope="col">#</th>
+							<th scope="col">Name</th>
+							<th scope="col">Gender</th>
+							<th scope="col">Email</th>
+							<th scope="col">Password</th>
+							<th scope="col">Address</th>
+							<th scope="col">Zip Code</th>
+							<th scope="col">Hobbies</th>
+						</tr>
+					</thead>
+					<tbody>
+						{usersList.length === 0 ? (
+							<tr>
+								<td colSpan="8" className="text-center">
+									No Records Found
+								</td>
+							</tr>
+						) : (
+							usersList.map((user, index) => (
+								<tr key={index}>
+									<th scope="row">{index + 1}</th>
+									<td>{user.name}</td>
+									<td>{user.gender}</td>
+									<td>{user.email}</td>
+									<td>{user.password}</td>
+									<td>{user.address}</td>
+									<td>{user.zipCode}</td>
+									<td>{user.hobbies}</td>
+								</tr>
+							))
+						)}
+					</tbody>
+				</table>
+			</div>
+		</>
+	);
+}
+
+export default Records;
